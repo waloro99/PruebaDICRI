8c7f03da49a75658e5042f718b03d980838505d0
waloro99 <walterof1@hotmail.com>
2025-11-21 14:52:32 -0600

    Se agrego contenedores docker

--- dicri-backend/src/db/db.config.js
+++ dicri-backend/src/db/db.config.js
@@ -1,16 +1,28 @@
 const sql = require('mssql');
 require('dotenv').config();
 
+// const dbConfig = {
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   server: process.env.DB_SERVER,
+//   database: process.env.DB_DATABASE,
+//   port: parseInt(process.env.DB_PORT, 10),
+//   options: {
+//     encrypt: process.env.DB_ENCRYPT === 'true',
+//     trustServerCertificate: true
+//   }
+// };
+
 const dbConfig = {
+  server: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 1433),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER,
-  database: process.env.DB_DATABASE,
-  port: parseInt(process.env.DB_PORT, 10),
+  database: process.env.DB_NAME,
   options: {
-    encrypt: process.env.DB_ENCRYPT === 'true',
-    trustServerCertificate: true
-  }
+    encrypt: false,
+    trustServerCertificate: true,
+  },
 };
 
 const poolPromise = new sql.ConnectionPool(dbConfig)
